b752bbc0ec31bf4791fcc1a3d8f07425e264254a
aniqfakhrul <aniqfakhrul@mcoba.org>
2021-05-18 13:26:46 +0000

    Updated

--- gename.py
+++ gename.py
@@ -4,17 +4,15 @@ domain_name = "@company.local"
 # Naming Function
 def gen_names(names, list_names):
     conv_list = []
-    list_rules = open("username.rules").readlines()
+    list_rules = open("usernames.rule").readlines()
     found_mid = False
     for j in list_rules:
         conv_list.append(j)
     if len(names.split(" ")) > 2:
         first,mid,last = names.split(" ")
-        print(first,mid,last)
         found_mid = True
     else:
         first,last = names.split(" ")
-        print(first,last)
     for rule in list_rules:
         try:
             if ("mid" in rule) and (not found_mid):
@@ -24,16 +22,21 @@ def gen_names(names, list_names):
         except:
             continue
 
-# Variables
-list_names = []
+def main():
+    # Variables
+    list_names = []
 
-# Loop
-for i in names_list:
-    gen_names(i.strip(),list_names)
+    # Loop
+    for i in names_list:
+        gen_names(i.strip().lower(),list_names)
+    print("[+] Saved: list_users.txt")
 
-# Save in a file
-f = open("list_user.txt", "w")
-for lines in list_names:
-    f.write(lines)
-    f.write("\n")
-f.close()
+    # Save in a file
+    f = open("list_user.txt", "w")
+    for lines in list_names:
+        f.write(lines)
+        f.write("\n")
+    f.close()
+
+if __name__ == "__main__":
+    main()
