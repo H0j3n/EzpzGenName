af06e44fe1d583487c8529a2d4d3743e269d426d
root <root@kali>
2021-05-19 01:47:28 +0000

    New update

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # EzpzGenName
+
+# What is EzpzGenName?
+
+**Disclaimer: This tool is not a bruteforcing tool and does not perform network-level authentication.**
+
+EzpzGenName is a tool written in Python by @ and @ which we encounter some scenario that needed to generate custom principal names to bruteforcing valid users in Keberoast. What makes EzpzGenName differents, you can specify your own custom.rule file *(Refer default file : usernames.rule)*
+
+This tool will be updated from time to time with any new rules we found. Feels free to make a PR to make EzpzGenName better.
+
+
+# Usage
+
+### 1. Usernames only
+
+```
+# Usage
+python3 gename.py -n user.txt -r usernames.rule
+
+# Output
+alex-james
+```
+
+### 2. Formatting
+
+- New Format
+
+```
+# Usage
+python3 gename.py -n user.txt -r usernames.rule -d "company.local" -f new
+
+# Output
+alex-james@company.local
+```
+
+- Old Format
+
+```
+# Usage
+python3 gename.py -n user.txt -r usernames.rule -d "company.local" -f old
+
+# Output
+company.local\alex-james
+```
--- gename.py
+++ gename.py
@@ -1,10 +1,29 @@
-names_list = open("user.txt").readlines()
-domain_name = "@company.local"
+import argparse
+parser = argparse.ArgumentParser(description='A tool that can generate User Principal Name with Custom Rules written in Python')
+parser.add_argument('-n','--names', help='Read list of names line by line.',required=True)
+parser.add_argument('-r','--rules', help='Read list of rules line by line.',default="usernames.rule")
+parser.add_argument('-d','--domain',help='Add domain if available')
+parser.add_argument('-f','--format',help='Formatting to use new or old format',default="new")
+
+args = parser.parse_args()
+txt_names = args.names
+newFormat = False
+if args.domain:
+    if args.format == "new":
+        domain_name = "@"+args.domain
+        newFormat = True
+    else:
+        domain_name = args.domain + "\\"
+else:
+    domain_name = ""
+txt_rules = args.rules
+
+names_list = open(txt_names).readlines()
 
 # Naming Function
 def gen_names(names, list_names):
     conv_list = []
-    list_rules = open("usernames.rule").readlines()
+    list_rules = open(txt_rules).readlines()
     found_mid = False
     for j in list_rules:
         conv_list.append(j)
@@ -34,7 +53,10 @@ def main():
     # Save in a file
     f = open("list_user.txt", "w")
     for lines in list_names:
-        f.write(lines)
+        if newFormat:
+            f.write(lines+domain_name)
+        else:
+            f.write(domain_name+lines)
         f.write("\n")
     f.close()
 
